6a1bca9275b4b3fd15f12538a72292c8f7fd6d71
Hrashit2611/ <100829059+HARSHITPROcoder@users.noreply.github.com>
2023-06-17 16:35:36 +0530

    Add files via upload

---
+++ Rolling Dice/Dice.py
@@ -0,0 +1,33 @@
+import random  # InBuild Module.
+
+no = random.randint(0, 6)
+
+if no == 0:
+    print("|-------|")
+    print("|   1   |")
+    print("|-------|")
+
+if no == 1:
+    print("|-------|")
+    print("|   2   |")
+    print("|-------|")
+    
+if no == 2:
+    print("|-------|")
+    print("|   3   |")
+    print("|-------|")
+    
+if no == 3:
+    print("|-------|")
+    print("|   4   |")
+    print("|-------|")
+    
+if no == 4:
+    print("|-------|")
+    print("|   5   |")
+    print("|-------|")
+    
+if no == 5:
+    print("|-------|")
+    print("|   6   |")
+    print("|-------|")
